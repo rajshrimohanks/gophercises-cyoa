e6bdb8e45d014fdf4edd02d2e5fe50f899be9a39
Rajshri Mohan K S <rajshrimohanks@gmail.com>
2020-02-15 16:23:56 +0530

    Updated story type

--- story.go
+++ story.go
@@ -78,12 +78,16 @@ var defaultHandlerTmpl = `
     </style>
 </html>`
 
-func NewHandler(s Story) http.Handler {
-	return handler{s}
+func NewHandler(s Story, t *template.Template) http.Handler {
+	if t == nil {
+		t = tpl
+	}
+	return handler{s, t}
 }
 
 type handler struct {
 	s Story
+	t *template.Template
 }
 
 func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
